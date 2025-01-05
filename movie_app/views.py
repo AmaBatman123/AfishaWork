1c3c8160f59d7bfc98007df24ea5a7967c6c02b5
Valerii <amabatman.12@gmail.com>
2025-01-05 21:22:18 +0600

    Added exeptions

--- movie_app/views.py
+++ movie_app/views.py
@@ -7,7 +7,12 @@ from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
 @api_view(http_method_names=['GET', 'POST'])
 def directors_list(request):
     if request.method == 'GET':
-        directors = Director.objects.all()
+        try:
+            directors = Director.objects.all()
+        except directors.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND,
+                            data={'message': 'Directors not found'})
+
         data_directors = DirectorSerializer(instance=directors, many=True).data
         return Response(data_directors)
     elif request.method == 'POST':
@@ -21,7 +26,12 @@ def detail_director_view(request, id):
 
 @api_view(http_method_names=['GET'])
 def movies_list(request):
-    movies = Movie.objects.all()
+    try:
+        movies = Movie.objects.all()
+    except Movie.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={'message': 'Movies not found'})
+
     data_movies = MovieSerializer(instance=movies, many=True).data
     return Response(data_movies)
 
@@ -33,7 +43,12 @@ def detail_movie_view(request, id):
 
 @api_view(http_method_names=['GET'])
 def reviews_list(request):
-    reviews = Review.objects.all()
+    try:
+        reviews = Review.objects.all()
+    except Review.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND,
+                        data={'message': 'Reviews not found'})
+
     data_reviews = ReviewSerializer(instance=reviews, many=True).data
     return Response(data_reviews)
 

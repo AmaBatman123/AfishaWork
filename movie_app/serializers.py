67f14e721bc1da6599f6d57c0dd0dd8994b9d089
Valerii <amabatman.12@gmail.com>
2025-01-05 21:03:11 +0600

    added serializers

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -3,3 +3,15 @@ from .models import Director, Movie, Review
 
 class DirectorSerializer(serializers.ModelSerializer):
     class Meta:
+        model = Director
+        fields = 'id name'.split()
+
+class MovieSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Movie
+        fields = 'id title description duration director'.split()
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = 'id text'.split()
--- movie_app/views.py
+++ movie_app/views.py
@@ -2,18 +2,13 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
 from .models import Director, Movie, Review
+from .serializers import DirectorSerializer, MovieSerializer, ReviewSerializer
 
 @api_view(http_method_names=['GET', 'POST'])
 def directors_list(request):
     if request.method == 'GET':
         directors = Director.objects.all()
-        data_directors = [
-            {
-                "id": director.id,
-                "name": director.name
-            }
-            for director in directors
-        ]
+        data_directors = DirectorSerializer(instance=directors, many=True).data
         return Response(data_directors)
     elif request.method == 'POST':
         pass
@@ -22,29 +17,12 @@ def directors_list(request):
 @api_view(http_method_names=['GET'])
 def movies_list(request):
     movies = Movie.objects.all()
-    data_movies = [
-        {
-            "id": movie.id,
-            "title": movie.title,
-            "description": movie.description,
-            "duration": movie.duration,
-            "director": movie.director.name,
-
-        }
-        for movie in movies
-    ]
+    data_movies = MovieSerializer(instance=movies, many=True).data
     return Response(data_movies)
 
 @api_view(http_method_names=['GET'])
 def reviews_list(request):
     reviews = Review.objects.all()
-    data_reviews = [
-        {
-            "id": review.id,
-            "text": review.text,
-            "movie": review.movie.title,
-        }
-        for review in reviews
-    ]
+    data_reviews = ReviewSerializer(instance=reviews, many=True).data
     return Response(data_reviews)
 

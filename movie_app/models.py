c99ec947993ac7fb7c1f2a5abac43ddfd595de79
Valerii <amabatman.12@gmail.com>
2025-01-08 22:24:04 +0600

    Added stars field to model

--- movie_app/models.py
+++ movie_app/models.py
@@ -1,4 +1,7 @@
+from email.policy import default
+
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Director(models.Model):
     name = models.CharField(max_length=100)
@@ -18,6 +21,13 @@ class Movie(models.Model):
 class Review(models.Model):
     text = models.TextField()
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
+    stars = models.IntegerField(
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(5)
+        ],
+        default = 0
+    )
 
     def __str__(self):
         return f"Review for {self.movie.title}"

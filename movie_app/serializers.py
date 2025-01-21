d608abcf968292cf86a0768a7daab5b59e6355af
Valerii <amabatman.12@gmail.com>
2025-01-21 15:23:09 +0600

    Added validation to movie description and finished validation

--- movie_app/serializers.py
+++ movie_app/serializers.py
@@ -8,37 +8,42 @@ class DirectorSerializer(serializers.ModelSerializer):
         model = Director
         fields = 'id name movie_count'.split()
 
-        def validate_name(self, value):
-            if len(value) < 3:
-                raise serializers.ValidationError("Имя режиссера должно быть более 3 символов")
-            return value
+    def validate_name(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Имя режиссера должно быть более 3 символов")
+        return value
 
 class MovieSerializer(serializers.ModelSerializer):
     class Meta:
         model = Movie
         fields = 'id title description duration director'.split()
 
-        def validate_title(self, value):
-            if len(value) < 3:
-                raise serializers.ValidationError("Название фильма должно быть не менее 3 символов.")
-            return value
+    def validate_title(self, value):
+        if len(value) < 3:
+            raise serializers.ValidationError("Название фильма должно быть не менее 3 символов.")
+        return value
 
-        def validate_duration(self, value):
-            if value <= 0:
-                raise serializers.ValidationError("Продолжительность фильма должна быть положительным числом.")
-            return value
+    def validate_descriprion(self, value):
+        if len(value) < 1:
+            raise serializers.ValidationError("Описание фильма должно быть не менее 1 символа.")
+        return value
+
+    def validate_duration(self, value):
+        if value <= 0:
+            raise serializers.ValidationError("Продолжительность фильма должна быть положительным числом.")
+        return value
 
 class ReviewSerializer(serializers.ModelSerializer):
     class Meta:
         model = Review
         fields = '__all__'
 
-        def validate_stars(self, value):
-            if value < 1 or value > 5:
-                raise serializers.ValidationError("Рейтинг должен быть от 1 до 5.")
-            return value
+    def validate_stars(self, value):
+        if value < 1 or value > 5:
+            raise serializers.ValidationError("Рейтинг должен быть от 1 до 5.")
+        return value
 
-        def validate_text(self, value):
-            if len(value) < 10:
-                raise serializers.ValidationError("Текст отзыва должен быть не менее 10 символов.")
-            return value
+    def validate_text(self, value):
+        if len(value) < 10:
+            raise serializers.ValidationError("Текст отзыва должен быть не менее 10 символов.")
+        return value
